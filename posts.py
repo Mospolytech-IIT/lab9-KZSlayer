448c816f7d668b6e3acd1248e45cf5f5914a0a7c
KZSlayer <nokitakaze@gmail.com>
2024-12-21 18:37:48 +0300

    Done Lab 9 Part 2

--- posts.py
+++ posts.py
@@ -16,3 +16,23 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(database.get_db)
 def list_posts(db: Session = Depends(database.get_db)):
     """Функция для получения списка всех Post."""
     return crud.get_posts(db)
+
+@router.post("/bulk", response_model=list[schemas.PostOut])
+def create_multiple_posts(posts: list[schemas.PostCreate], db: Session = Depends(database.get_db)):
+    """Функция для добавления нескольких постов."""
+    return crud.add_multiple_posts(db, posts)
+
+@router.get("/all", response_model=list[schemas.PostOut])
+def get_all_posts_with_users(db: Session = Depends(database.get_db)):
+    """Функция для получения всех постов с пользователями."""
+    return crud.get_all_posts_with_users(db)
+
+@router.put("/{post_id}/content")
+def update_post_content(post_id: int, new_content: str, db: Session = Depends(database.get_db)):
+    """Функция для обновления content поста."""
+    return crud.update_post_content(db, post_id, new_content)
+
+@router.delete("/{post_id}")
+def delete_post(post_id: int, db: Session = Depends(database.get_db)):
+    """Функция для удаления поста."""
+    return crud.delete_post(db, post_id)
--- users.py
+++ users.py
@@ -16,3 +16,23 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)
 def list_users(db: Session = Depends(database.get_db)):
     """Функция для получения списка всех пользователей."""
     return crud.get_users(db)
+
+@router.post("/bulk", response_model=list[schemas.UserOut])
+def create_multiple_users(users: list[schemas.UserCreate], db: Session = Depends(database.get_db)):
+    """Функция для добавления нескольких пользователей."""
+    return crud.add_multiple_users(db, users)
+
+@router.get("/all", response_model=list[schemas.UserOut])
+def get_all_users(db: Session = Depends(database.get_db)):
+    """Функция для получения всех пользователей."""
+    return crud.get_all_users(db)
+
+@router.put("/{user_id}/email")
+def update_user_email(user_id: int, new_email: str, db: Session = Depends(database.get_db)):
+    """Функция для обновления email пользователя."""
+    return crud.update_user_email(db, user_id, new_email)
+
+@router.delete("/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(database.get_db)):
+    """Функция для удаления пользователя и его постов."""
+    return crud.delete_user_and_posts(db, user_id)
